158764cfd7d8c5b7b948ae4083776a78b436a60f
Jesse V. Burke <jburke@wapacklabs.com>
2019-02-18 20:36:15 -0500

    backup because instructor sucks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Header } from './shared/Header';
+import { RentalCard } from './components/rental/RentalCard';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +8,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
+        <div className='container'>
+          <section id='rentalListing'>
+            <h1 className='page-title'>Your Home All Around the World</h1>
+            <div className='row'>
+            <RentalCard/>
+            <RentalCard/>
+            <RentalCard/>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
